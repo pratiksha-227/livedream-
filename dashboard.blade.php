166120afa4067789d4c08fa8c539f32079234445
Pratiksha Terse <109690404+pratiksha-227@users.noreply.github.com>
2025-06-04 21:12:10 +0530

    Add files via upload

---
+++ dashboard.blade.php
@@ -0,0 +1,161 @@
+
+@extends('layouts.app')
+
+@section('content')
+<div class="container-fluid">
+
+<!-- Page Heading -->
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+    <!-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+            class="fas fa-download fa-sm text-white-50"></i> Generate Report</a> -->
+</div>
+
+<!-- Content Row -->
+<div class="d-flex flex-column">
+    <div class="row">
+
+        <!-- Earnings (Monthly) Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="d-flex  flex-column">
+                        <!-- Left: Image -->
+                        <div class="">
+                            <img src="img\Group 289551.png" alt="Icon"  width="60" height="60">
+                        </div>
+                        
+                       
+                        <div>
+                            <div class="h5 mb-1 font-weight-bold text-gray-800">500</div>
+                            <div class="text-xs font-weight-bold text-primary text-uppercase">
+                                <h5 class="mb-0">Total companies</h5>
+                            </div>
+                        </div>  
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+
+        <!-- Earnings (Monthly) Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-secondary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="d-flex  flex-column">
+                        <!-- Left: Image -->
+                        <div class="me-3">
+                            <img src="img\Group 289551 (1).png" alt="Icon"  width="60" height="60">
+                        </div>
+                        
+                       
+                        <div>
+                            <div class="h5 mb-1 font-weight-bold text-gray-800">10,00,000</div>
+                            <div class="text-xs font-weight-bold text-primary text-uppercase">
+                                <h5 class="mb-0">Total Product</h5>
+                            </div>
+                        </div>  
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Earnings (Monthly) Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="d-flex  flex-column">
+                        <!-- Left: Image -->
+                        <div class="me-3">
+                            <img src="img\Group 289551 (2).png" alt="Icon"  width="60" height="60">
+                        </div>
+                        
+                        <!-- Middle: Value -->
+                       
+                        <div>
+                            <div class="h5 mb-1 font-weight-bold text-gray-800">15</div>
+                            <div class="text-xs font-weight-bold text-primary text-uppercase">
+                                <h5 class="mb-0">Total Categories</h5>
+                            </div>
+                        </div>  
+
+                    </div>
+                </div>
+            </div>
+        </div>
+       
+    </div>
+    <div class="row">
+
+<!-- Earnings (Monthly) Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="d-flex  flex-column">
+                        <!-- Left: Image -->
+                        <div class="me-3">
+                            <img src="img\Group 289551 (3).png" alt="Icon"  width="60" height="60">
+                        </div>
+                        
+                       
+                        <div>
+                            <div class="h5 mb-1 font-weight-bold text-gray-800">10</div>
+                            <div class="text-xs font-weight-bold text-primary text-uppercase">
+                                <h5 class="mb-0">Total Adhesive</h5>
+                            </div>
+                        </div>  
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- Earnings (Monthly) Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-secondary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="d-flex  flex-column">
+                        <!-- Left: Image -->
+                        <div class="me-3">
+                            <img src="img\Group 289551 (4).png" alt="Icon"  width="60" height="60">
+                        </div>
+                        
+                        <!-- Middle: Value -->
+                        
+                        <div>
+                            <div class="h5 mb-1 font-weight-bold text-gray-800">4</div>
+                            <div class="text-xs font-weight-bold text-primary text-uppercase">
+                                <h5 class="mb-0">Total Zones</h5>
+                            </div>
+                        </div>  
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Earnings (Monthly) Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-success shadow h-100 py-2">
+            <div class="card-body p-3">
+                <div class="d-flex flex-column align-items-start ">
+                    <!-- Left: Image -->
+                    <div>
+                        <img src="img/Group 289552.png" alt="Icon" width="60" height="60">
+                    </div>
+                    
+                    <!-- Middle: Value + Text -->
+                    <div>
+                        <div class="h5 mb-1 font-weight-bold text-gray-800">4000</div>
+                        <div class="text-xs font-weight-bold text-primary text-uppercase">
+                            <h5 class="mb-0">Total Quotations</h5>
+                        </div>
+                    </div>  
+                </div>
+            </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+@endsection
